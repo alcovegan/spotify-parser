00b3037166d4e48f5e334ed05872f7d4263a81b8
Alexander Sharabarov <alcovegan@gmail.com>
2020-07-20 23:44:57 +0300

    chore: removed console.log :3

--- playlist-parser.js
+++ playlist-parser.js
@@ -18,7 +18,6 @@ const parseTXT = function() {
 	return fs.readFileSync("./playlist.txt", { encoding: "utf8" })
 		.split("\r\n")
 		.map((l, idx) => {
-			console.log(l);
 			return {
 				"q": l.replace(re, ""),
 				"order": idx + 1
